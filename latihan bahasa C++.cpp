5cd87d8e6bf14dd02bfae8ed047dd47765168269
IkramND <113400552+IkramND@users.noreply.github.com>
2022-09-13 21:48:53 +0700

    Add files via upload

---
+++ latihan bahasa C++.cpp
@@ -0,0 +1,3 @@
+#include <iostream>
+using namespace std;
+int main() {int; for(int a = 01; a <= 10 ; a++){cout<<"belajar coding di kelas algoritma"<<endl;}}
---
+++ latihan1 bahasa C.cpp
@@ -0,0 +1,3 @@
+#include <stdio.h>
+
+int main(){for(char i = '0'; i<='9'; i++) {printf ("belajar coding dikelas algoritma\n", i );}}
