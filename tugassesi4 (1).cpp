3d9818b0d22de8b446613a39487638e408a7dd30
IkramND <113400552+IkramND@users.noreply.github.com>
2022-10-05 22:42:04 +0700

    Add files via upload

---
+++ tugassesi4 (1).cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+string nama,hadiah;
+
+int nilai1,nilai2,nilai3,rata;
+
+int main()
+
+{
+  cout << "Program hitung nilai rata rata\n";
+
+ int x;
+
+ cout << "Nama siswa: "; 
+
+ cin >> nama; 
+
+ cout << "Nilai Pertandingan I: ";
+
+ cin >> nilai1;
+
+ cout << "Nilai Pertandingan II: ";
+
+ cin >> nilai2;
+
+ cout << "Nilai Pertandingan III: ";
+
+ cin >> nilai3;
+
+ rata = (nilai1+nilai2+nilai3)/3;
+
+ if(rata>=85){
+
+     hadiah="Komputer I5";
+
+ }else if(rata>=70){
+
+     hadiah="hadiah Uang sebesar Rp. 2.500,000";
+
+ }else{
+
+     hadiah="hadiah Hiburan";
+
+ }
+
+ 
+
+ cout << "siswa yang bernama " << nama <<" memperoleh nilai rata rata " << rata << " dari hasil perlombaannya hadiah yang didapatkannya adalah " << hadiah;
+
+ return 0;
+
+}
